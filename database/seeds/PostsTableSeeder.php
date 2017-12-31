0620648ed1daf128600621ddc30d3bb4eb72ccb2
陳姿穎 <roeyang1410@gmail.com>
2017-12-31 22:31:08 +0800

    練習4-3：使用 Faker 讓資料更擬真

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -15,10 +16,11 @@ class PostsTableSeeder extends Seeder
         //
         Post::truncate();
         $total = 20;
+        $faker = Faker::create('zh_TW');
         foreach(range(1, 20) as $number) {
             Post::create([
-                'title' => 'title '.$number,
-                'content' => 'content '.$number,
+                'title' => $faker->sentence,
+                'content' => $faker->paragraph,
                 'is_feature' => rand(0, 1),
                 'created_at' =>Carbon::now()->subDays($total - $number),
                 'updated_at' =>Carbon::now()->subDays($total - $number),
