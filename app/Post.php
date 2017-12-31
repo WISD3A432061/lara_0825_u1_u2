5f43511bcf83ffd9446603617f5a006bbf81f179
陳姿穎 <roeyang1410@gmail.com>
2017-12-31 21:02:54 +0800

    練習7-1：在Post.php增加關聯

--- app/Post.php
+++ app/Post.php
@@ -1,16 +1,18 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
+use App\Comment;
 class Post extends Model
 {
     //
-    protected $table = 'posts';
-    protected $fillable = [
+    protected $table='posts';
+    protected $fillable=[
         'title',
         'content',
         'is_feature'
-    ];
+        ];
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
